34720ee3e2a02a34d5c8c5e31585c1e631f7d09e
ashayaan <a.shayaan@gmail.com>
2019-03-28 14:57:47 +0530

    Proccesed Data

---
+++ data/process_data.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+
+def getFilesName(path):
+	return os.listdir(path)
+
+def readFiles(files):
+	data_frames = {}
+	for name in files:
+		if name.endswith('.csv'):
+			df = pd.read_csv(path+'/'+name)
+			data_frames[name] = df
+	return data_frames
+
+def calculateReturns(data_frames):
+	for name in data_frames.keys():
+		data_frames[name]['Return'] =  (data_frames[name]['Price'] -  data_frames[name]['Open']) / data_frames[name]['Open']
+	return data_frames
+
+
+def mergeReturns(data_frames,files):
+	data = {}
+	for name in data_frames.keys():
+		column_name = name.split('.')[0]
+	 	data[column_name] = list(data_frames[name]['Return'])
+
+
+	df = pd.DataFrame.from_dict(data)
+	df['Date'] = data_frames['BHEL_Historical_Data.csv']['Date']
+	df.to_csv('combined_returns.csv',index=False)	
+
+if __name__ == '__main__':
+	path = 'daily_data'
+	files = getFilesName(path)
+
+	data_frames = readFiles(files)	
+
+	data_frames = calculateReturns(data_frames)
+
+	mergeReturns(data_frames,files)
